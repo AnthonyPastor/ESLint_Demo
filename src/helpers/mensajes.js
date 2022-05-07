22cdae865eeb0e996e188b8277d9a6765066263f
Anthony <nahuel1pastor@gmail.com>
2022-05-07 19:58:23 -0300

    fix: Spaces in Mensajes

--- src/helpers/mensajes.js
+++ src/helpers/mensajes.js
@@ -2,45 +2,42 @@ require('colors');
 
 const mostrarMenu = () => {
 
-  return new Promise( resolve => {
-    console.clear();
-    console.log("========================".green);
-    console.log(" Seleccione una opción".green);
-     console.log("========================\n".green);
+    return new Promise( resolve => {
+        console.clear();
+        console.log("========================".green);
+        console.log(" Seleccione una opción".green);
+         console.log("========================\n".green);
   
-    console.log(`${"1.".green} Mostrar Hola Mundo.`);
-    console.log(`${"2.".green} Mostrar Persona`);
-    console.log(`${"0.".green} Salir \n`);
+        console.log(`${"1.".green} Mostrar Hola Mundo.`);
+        console.log(`${"2.".green} Mostrar Persona`);
+        console.log(`${"0.".green} Salir \n`);
   
-    const readline = require("readline").createInterface({
-      input: process.stdin,
-      output: process.stdout
+        const readline = require("readline").createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+  
+        readline.question("Seleccione una opción: ", (opt) => {
+            readline.close();
+            resolve(opt);
+        });
+  
     });
-  
-    readline.question("Seleccione una opción: ", (opt) => {
-      readline.close();
-      resolve(opt);
-    });
-  
-  });
-  
 };
 
 const pausa = () => {
 
-  return new Promise(resolve => {
+    return new Promise(resolve => {
+        const readline = require("readline").createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
 
-    const readline = require("readline").createInterface({
-      input: process.stdin,
-      output: process.stdout
+        readline.question(`\nPresione ${"ENTER".green} para continuar\n`, () => {
+            readline.close();
+            resolve();
+        });
     });
-
-    readline.question(`\nPresione ${"ENTER".green} para continuar\n`, () => {
-      readline.close();
-      resolve();
-    });
-  });
- 
 };
 
 module.exports = {
